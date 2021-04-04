05c8960515dd4c0690b8ed27b17e2250fb80137a
ayoubft <63267601+ayoubft@users.noreply.github.com>
2021-04-05 00:51:37 +0200

    Added helloX.py

---
+++ hello_X.py
@@ -0,0 +1,11 @@
+# ayoubft
+##############
+##############
+# Here I will be trying to make a function
+# that return hello plus the name of the user
+#############################################
+#############################################
+
+def hello_x(a):
+    pass
+
